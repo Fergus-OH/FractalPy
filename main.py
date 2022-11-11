dce2d1c7dc34f107ebc588e2b75126a3526df49f
FergusOHan <77754538+Fergus-OH@users.noreply.github.com>
2022-11-11 18:19:40 +0000

    Small clean on main.py

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ def main():
     x_init = (-3, 3)
     y_init = (-1.6875, 1.6875)
 
-    # target = (62407000, -0.743643930055, -0.131825895901)
     target = (10004407000, -0.7436439059192348, -0.131825896951)
 
-    (m, x, y) = target
-    x_l = abs(x_init[1] - x_init[0]) / m
-    x_target = (x - x_l / 2, x + x_l / 2)
-    y_l = abs(y_init[1] - y_init[0]) / m
-    y_target = (y - y_l / 2, y + y_l / 2)
+
+
     # plot target
     # Mandelbrot(x_ran=x_target, y_ran=y_target, threshold=5000).plot(dpi=100, axis='on')
     # Fractal(x_ran=x_init, y_ran=y_init, threshold=5000, n_pts=360, pallet_len=25).zoom(n_frames=100, target=target)
